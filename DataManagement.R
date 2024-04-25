4ff717f2907c00f12b3205e75192d78de34ed0d7
samgarciaperez <91906315+samgarciaperez@users.noreply.github.com>
2024-04-25 14:40:01 -0700

    hello

--- DataManagement.R
+++ DataManagement.R
@@ -1,3 +1,6 @@
 #############################################################################
 # NAME: DATA MANAGEMENT 
 
+# ok lets try this again, can you see this 
+
+data <- data.frame(c(1), c(1))
